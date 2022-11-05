e61ee452ca98215f56b639137c8f90cc29eb6949
namu0309 <115522670+namu0309@users.noreply.github.com>
2022-11-05 16:40:56 +0900

    set과 중력에 대해 이해

--- mover.js
+++ mover.js
@@ -2,17 +2,18 @@ class Mover {
     constructor(x,y) {
         this.pos = createVector(x,y);
         this.vel = createVector(0,0);
-        this.acc = createVector(0,0.1); 
+        this.acc = createVector(0,0); 
         this.w = 20;
     }
     
     applyForce(force) {
-        this.acc = force;
+        this.acc.add(force);
     }
 
     update() {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
+        this.acc.set(0,0);
     }
 
     edge() {
--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let mover
 function setup() {
   createCanvas(400, 400);
   background(0);
-  mover = new mover(width/2,height/2);
+  mover = new Mover(width/2,height/2);
 
 }
 
@@ -13,7 +13,7 @@ function draw() {
 
   let gravity = createVector(0,0.1);
 
-  mover.applyForce(gravity)
+  mover.applyForce(gravity);
   mover.update();
   mover.show();
   mover.edge();
